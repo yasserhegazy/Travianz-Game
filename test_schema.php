491d9e0e3939c8e30c94163fc19adeb68b399f7c
yasserhegazy <yasseranahegazy@gmail.com>
2026-05-05 10:13:20 +0300

    refactor: remove legacy development scripts and assets while updating docker configuration

--- test_schema.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("GameEngine/Database.php");
-$res = mysqli_query($database->dblink, "SHOW COLUMNS FROM " . TB_PREFIX . "users");
-while($row = mysqli_fetch_assoc($res)) {
-    echo $row['Field'] . "<br>";
-}
-?>
